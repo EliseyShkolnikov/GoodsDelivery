37c02b5e67a91b20282b5594a37f7cbd78bd8315
Crampass <eliseymrchirik@gmail.com>
2020-04-23 23:33:53 +0300

    update

--- Main.py
+++ Main.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('base.html')
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 
 if __name__ == '__main__':
