58515a7b68a764abb6a1b3759dcf61ea957fe7dd
Crampass <eliseymrchirik@gmail.com>
2020-04-24 22:50:35 +0300

    Update Base(add links)

--- Main.py
+++ Main.py
@@ -8,6 +8,15 @@ def index():
     return render_template('base.html')
 @app.route('/contacts')
 def contacts():
+    return render_template('goods.html')
+@app.route('/goods')
+def contacts():
+    return render_template('couriers.html')
+@app.route('/couriers')
+def contacts():
+    return render_template('about.html')
+@app.route('/about')
+def contacts():
     return render_template('contacts.html')
 
 
