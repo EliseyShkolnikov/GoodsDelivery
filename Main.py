ee2d186114190df15e9fae6f5704d11dfdfd9911
Crampass <eliseymrchirik@gmail.com>
2020-04-24 23:30:42 +0300

    Добавил логин и регистрацию

--- Main.py
+++ Main.py
@@ -29,8 +29,10 @@ def contacts():
    
 @app.route('/login')
 def login():
+    return render_template('login.html')
+@app.route('/registration')
+def registration():
     return render_template('registration.html')
-
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0', debug=True)
