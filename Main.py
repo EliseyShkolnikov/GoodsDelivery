f79ad6c143ddddf8f1805e8a9eabb2cb80ced851
Crampass <eliseymrchirik@gmail.com>
2020-05-06 02:08:48 +0300

    Исправил ошибку с бд. Серега, радуйся

--- Main.py
+++ Main.py
@@ -53,21 +53,11 @@ def delete(ID):
     db.session.commit()
     return redirect(url_for('index'))
 
-
+# Вот тут кидаем в бд данные пользователя
 @app.route('/create-task', methods=['POST'])
 def create():
-    new_task1 = GG(Name=request.form['name'])
-    new_task2 = GG(Address=request.form['address'])
-    new_task3 = GG(Goods=request.form['goods'])
-    new_task4 = GG(Period=request.form['period'])
-    new_task5 = GG(Coment=request.form['coment'])
-    new_task6 = GG(Photo=request.form['photo'])
-    db.session.add(new_task1)
-    db.session.add(new_task2)
-    db.session.add(new_task3)
-    db.session.add(new_task4)
-    db.session.add(new_task5)
-    db.session.add(new_task6)
+    new_task = GG(Name=request.form['name'], Address=request.form['address'], Goods=request.form['goods'], Period=request.form['period'], Coment=request.form['coment'],Photo=request.form['photo'])
+    db.session.add(new_task)
     db.session.commit()
     return redirect(url_for('contacts1'))
 
